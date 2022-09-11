90d926aa55807e135e07e4a36aae279ba8feda88
jontcont <52921053+JontCont@users.noreply.github.com>
2022-09-11 13:36:36 +0800

    update Reply Message

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using isRock.LineBot;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -6,7 +7,7 @@ namespace dotnetCore_LineBot.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class MessageController
+    public class MessageController: ControllerBase
     {
         public string channelToken{get; private set;}
             =@"/MuwA0g7s5mT9vBzzihsVH0JigeA2dQSO1ETashy34az/yc8j1BGS5/CSzPQionIXwfliqhEVcFCctom2S50La+uPAvK85miNgx8ZaYJdmhkeFDfSvlQ0+A9r9gp3fnAXAbYWKTgxuWMqFpXevvFmQdB04t89/1O/w1cDnyilFU=";
@@ -16,7 +17,34 @@ namespace dotnetCore_LineBot.Controllers
         [HttpPost]
         public string Post(){
             Bot bot = InitialBots();
-            bot.PushMessage(adminUserID, "Push Message !!");
+            List<MessageBase> repMessage = new List<MessageBase>();
+            StringBuilder strBuilder = new StringBuilder();
+            string strBody = "";
+            
+            try{
+                //取得 http Post 
+                using (StreamReader reader = new(Request.Body, System.Text.Encoding.UTF8))
+                {
+                    strBody = reader.ReadToEndAsync().Result;
+                    if (reader == null || string.IsNullOrEmpty(strBody))
+                        return JsonConvert.SerializeObject(new { success = false, message = "error : message empty " });
+                }
+            }catch(Exception ex){
+                bot.PushMessage(adminUserID, ex.Message);
+                return JsonConvert.SerializeObject(new { success = false, message = ex.Message });
+            }
+
+            //RawData(should be JSON)
+            var ReceivedMessage = Utility.Parsing(strBody);
+            if (ReceivedMessage == null) return JsonConvert.SerializeObject(new { success = false, message = "error : message empty " });
+
+            var LineEvent = ReceivedMessage.events.FirstOrDefault();
+            if (LineEvent == null)
+            {
+                return JsonConvert.SerializeObject(new { success = false, message = "error : not found event ! " });
+            }
+            ReplyBotsMessage(bot,LineEvent);
+
             return JsonConvert.SerializeObject(new { success = true, message = "" });
         }
 
@@ -27,6 +55,26 @@ namespace dotnetCore_LineBot.Controllers
             return new Bot(channelToken);
         }
 
+        private void ReplyBotsMessage(Bot bot,Event lineEvent)
+        {
+            TextMessage textMessage = new ("");
+
+            switch (lineEvent.type)
+            {
+                case "join":
+                    textMessage = new TextMessage($"大家好啊~");
+                    break;
+                case "message":
+                    string text = lineEvent.message.text;
+                    if (text == null) break;
+                    textMessage =  new TextMessage($"您回應是 : {text}");
+                    break;
+            }
+            if(string.IsNullOrEmpty(textMessage.text)){
+                textMessage = new ($"你回覆的訊息無法判讀，請重新輸入!!");
+            }
+            bot.ReplyMessage(lineEvent.replyToken, textMessage);
+        }
 
     }
 }
